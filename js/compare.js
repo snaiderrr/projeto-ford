b6d9569e2a1bc17e658ac6b8b989aac7001216d4
snaiderrr <filipekeven@hotmail.com>
2025-06-03 15:20:45 -0300

    Criando a classe no javascript

---
+++ js/compare.js
@@ -0,0 +1,16 @@
+class Car {
+    constructor(id, model, image, heightCargo, heightVehicle, groundClearance, cargoCapacity, engine, power, cargoVolume, wheels, price) {
+        this.id = id;
+        this.model = model;
+        this.image = image;
+        this.heightCargo = heightCargo;
+        this.heightVehicle = heightVehicle;
+        this.groundClearance = groundClearance;
+        this.cargoCapacity = cargoCapacity;
+        this.engine = engine;
+        this.power = power;
+        this.cargoVolume = cargoVolume;
+        this.wheels = wheels;
+        this.price = price;
+    }
+}
